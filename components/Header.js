ab1af59230caebfb694933c210d1a2754b43490a
Darshan Mevada <darshan-mevada@mavenprofserv.com>
2025-02-05 13:07:30 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='flex justify-center items-center h-20 bg-gray-800 text-white'>
+            custome App
+        </div>
+    )
+}
+
+export default Header
